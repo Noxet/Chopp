17d8d4d42299ecb00d337a049429050af11a6faf
Jonathan Sönnerup <nooxet@gmail.com>
2021-11-13 00:09:58 +0100

    Got background image loading

--- src/Chopp.cpp
+++ src/Chopp.cpp
@@ -6,8 +6,28 @@
 int main()
 {
 	sf::VideoMode vm(1920, 1080);
+	sf::RenderWindow window(vm, "Chopp!", sf::Style::Default);
 
-	sf::RenderWindow window(vm, "Chopp!", sf::Style::Fullscreen);
+	// A texture to hold a graphic on the GPU
+	sf::Texture bgTexture;
+	bgTexture.loadFromFile("../assets/gfx/background.png");
+
+	// Create a sprite, load the texture, and set it to cover the screen
+	sf::Sprite bgSprite;
+	bgSprite.setTexture(bgTexture);
+	bgSprite.setPosition(0, 0);
+
+	while (window.isOpen())
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		window.clear();
+		window.draw(bgSprite);
+		window.display();
+	}
 
 	return 0;
 }
